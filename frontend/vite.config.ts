8de7754c30932e9275adca8f537ed3ce91c85ae7
zhengyi <admin@mczhengyi.top>
2024-03-27 19:50:59 +0800

    ⬆️ Update dependency

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,7 +10,7 @@ const viteCompressionFilter = /\.(js|mjs|json|css|html|svg)$/i;
 // https://vitejs.dev/config/
 export default defineConfig({
     server: {
-        port: 5000,
+        port: 5002,
     },
     define: {
         "FRONTEND_VERSION": JSON.stringify(process.env.npm_package_version),
